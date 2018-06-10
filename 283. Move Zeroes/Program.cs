1363bc6d8fa6b9ea94d50ac6e518060b426a4770
jsw7524 <jsw7524@gmail.com>
2018-06-10 23:20:37 +0800

    AC

--- 283. Move Zeroes/Program.cs
+++ 283. Move Zeroes/Program.cs
@@ -6,6 +6,30 @@ using System.Threading.Tasks;
 
 namespace _283.Move_Zeroes
 {
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int lenZero = 0;
+            int i = 0;
+            for (;i < nums.Length;i++)
+            {
+                if (0 == nums[i])
+                {
+                    lenZero++;
+                }
+                else
+                {
+                    nums[i - lenZero] = nums[i];
+                }
+            }
+            for (i=i- lenZero; i< nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using _283.Move_Zeroes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution sln = new Solution();
+            int[] testData = new int[] { 0, 1, 0, 3, 12 };
+            sln.MoveZeroes(testData);
+            int[] ans = new int[] { 1, 3, 12, 0, 0 };
+            Assert.AreEqual(JsonConvert.SerializeObject(testData), JsonConvert.SerializeObject(ans));
+
+
+        }
+    }
+}
